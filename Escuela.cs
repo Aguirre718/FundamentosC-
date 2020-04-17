4afc3a281084f70264de03d5f9303b375b7c7abd
Camilo Aguirre <camiloaguirre24@gmail.com>
2020-04-17 11:28:40 -0500

    Curso Plazi C# video 9

---
+++ Escuela.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core_Escuela
+{
+    class Escuela
+    {
+        public string name;
+        public string address;
+        public string year;
+        public string ceo;
+
+        public void beep()
+        {
+            Console.Beep(1000, 2000);
+            Console.WriteLine("¡Already rang!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Core_Escuela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.name = "Pedro Luis Villa";
+            miEscuela.address = "Crr 37 # 66D - 18";
+            miEscuela.beep();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
